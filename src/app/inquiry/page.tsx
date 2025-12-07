218e463d831dddad6547429a63a47662bce50056
hyunyeee <5004ohy@naver.com>
2025-12-07 22:36:22 +0900

    feat: 문의하기 Form 컴포넌트 구현

--- src/app/inquiry/page.tsx
+++ src/app/inquiry/page.tsx
@@ -1,6 +1,6 @@
 import { DimmedImageBanner } from "@/components/DimmedImageBanner";
 import { SectionWrapper } from "@/components/SectionWrapper";
-import Link from "next/link";
+import CustomerInfoForm from "@/components/form/contact/CustomerInfoForm";
 
 export default function Inquiry() {
   return (
@@ -11,14 +11,7 @@ export default function Inquiry() {
       />
       <SectionWrapper type="white">
         <h1 className="text-xl font-bold">고객 정보</h1>
-        여기에 폼이 들어가야함
-        <Link
-          href="https://xn--oy2b27nt4dhvc.com/car/inquiry.html"
-          target="_blank"
-          className="text-main block underline"
-        >
-          이거참고
-        </Link>
+        <CustomerInfoForm />
       </SectionWrapper>
     </main>
   );
