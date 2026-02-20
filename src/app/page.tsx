08c63b9d1b4c8558ced24e194c74fd0058cfbd33
hyunyeee <5004ohy@naver.com>
2026-02-20 17:05:17 +0900

    refactor: 불필요한 섹션 주석 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,9 +70,9 @@ export default async function Home() {
         <VehiclesPreviewSection />
       </Suspense>
 
-      <FeatureGridSection />
-      <StackedCardsSection />
-      <ImageTripleSection />
+      {/*<FeatureGridSection />*/}
+      {/*<StackedCardsSection />*/}
+      {/*<ImageTripleSection />*/}
       <TimeLineSection />
       <FloatingCustomerForm />
     </main>
