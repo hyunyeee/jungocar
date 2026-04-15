ae36e09a77ed21cc46aebf5b80dd53e70aa05a40
hyunyeee <5004ohy@naver.com>
2026-04-15 15:44:46 +0900

    refactor: 불필요한 요청 제거

--- src/lib/api/customer.ts
+++ src/lib/api/customer.ts
@@ -16,16 +16,16 @@ export async function postCustomerInfo(payload: CustomerInfo) {
   return res.json();
 }
 
-export async function postMailCustomerInfo(payload: CustomerInfo) {
-  const res = await fetch(`${baseURL}/management/mail`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  });
-  if (!res.ok) {
-    throw new Error(`요청 실패: ${res.status}`);
-  }
-  return res.text();
-}
+// export async function postMailCustomerInfo(payload: CustomerInfo) {
+//   const res = await fetch(`${baseURL}/management/mail`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(payload),
+//   });
+//   if (!res.ok) {
+//     throw new Error(`요청 실패: ${res.status}`);
+//   }
+//   return res.text();
+// }
